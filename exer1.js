b51d4ee87705d496b0279012a1b5dcbfff2812f1
Marlon Couto <marloncouto199@gmail.com>
2022-09-23 17:43:54 -0300

    Conclui o primeiro exercício
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- exer1.js
+++ exer1.js
@@ -1,2 +1,9 @@
 // 1 - Desenvolva um algoritmo que calcula a soma dos números 1 até 50 usando a estrutura condicional "for". Retorne a resposta no seguinte formato:
 // A soma total de 1 até 50 é: X
+let somaTotal = 0;
+
+for (let index = 1; index < 51; index += 1) {
+  somaTotal += index;
+} 
+
+console.log(`A soma total de 1 até 50 é ${somaTotal}`);
