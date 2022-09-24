a2fd33bab66c6812abffdc04df3c81ed73109d15
Marlon Couto <marloncouto199@gmail.com>
2022-09-23 21:09:30 -0300

    Formata exer3.js com o Prettier
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- exer3.js
+++ exer3.js
@@ -7,17 +7,16 @@ let jogador1 = possibilidades[jogada1];
 let jogada2 = Math.floor(Math.random() * 2);
 let jogador2 = possibilidades[jogada2];
 
-
-
-if (jogador1 == 'tesoura' && jogador2 == 'pedra' || jogador1 == 'pedra' && jogador2 == 'papel' || jogador1 == 'papel' && jogador2 == 'tesoura')
-{
-    console.log('Jogador 2 VENCEU!');
-}else if(jogador1 == jogador2)
-{
-    console.log('EMPATE!');
-}else
-{
-    console.log('Jogador 1 VENCEU');
+if (
+  (jogador1 == 'tesoura' && jogador2 == 'pedra') ||
+  (jogador1 == 'pedra' && jogador2 == 'papel') ||
+  (jogador1 == 'papel' && jogador2 == 'tesoura')
+) {
+  console.log('Jogador 2 VENCEU!');
+} else if (jogador1 == jogador2) {
+  console.log('EMPATE!');
+} else {
+  console.log('Jogador 1 VENCEU');
 }
 console.log(`O jogador Um escolheu: ${jogador1}`);
 console.log(`O jogador Dois escolheu: ${jogador2}`);
